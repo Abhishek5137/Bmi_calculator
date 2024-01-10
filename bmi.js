a8796d4da9a59a09c69f4fb86f49b142f6327a16
Abhishek5137 <111166632+Abhishek5137@users.noreply.github.com>
2024-01-10 12:21:22 +0530

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,19 @@
+const form =document.querySelector('form')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+ const height =parseInt( document.querySelector('#height').value)
+ const weight = parseInt(document.querySelector('#weight').value)
+ const results = document.querySelector('#results');
+ if(height ===''  ||height <0 || isNaN(height)){
+    results.innerHTML=`Enter Valid Height ${height}`;
+ }
+ else if(weight ===''  ||weight <0 || isNaN(weight)){
+    results.innerHTML=`Enter Valid weight ${weight}`;
+ }
+else {
+    
+    const bmi=(weight/((height*height)/10000)).toFixed(2)
+    results.innerHTML=`Bmi ${bmi}`
+}
+});
